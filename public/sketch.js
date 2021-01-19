40b92502e42d236535bd43a41f6a5fbd2552a19f
donatorenzulli <51850471+donatorenzulli@users.noreply.github.com>
2021-01-19 11:03:13 +0100

    Con punteggio totale

--- public/sketch.js
+++ public/sketch.js
@@ -3,91 +3,110 @@ const increment = firebase.firestore.FieldValue.increment(1);
 let socket = io();
 socket.on("mousedxBroadcast", clickdx);
 socket.on("mousesxBroadcast", clicksx);
-let penne= db.collection('pasta').doc('penne')
-
 
 let winner;
 let punteggiopenne = 0;
 let punteggiospaghetti = 0;
 let timer = 5;
+let scorespaghetti = []
+let scorepenne = [];
 
 
 
-function setup (){
-let c = createCanvas(windowWidth,250)
-c.position(0,0)
-setInterval(timerdown, 1000)
+function setup() {
+  let c = createCanvas(windowWidth, 250)
+  c.position(0, 0)
+  setInterval(timerdown, 1000)
+    textAlign(CENTER)
 }
 
 
 
 
-function renderpasta(doc){
+function renderpasta(doc) {
   let li = document.createElement('li');
   let name = document.createElement('span');
   let punteggio = document.createElement('span');
 
-    li.setAttribute('data-id', doc.id);
-    name.textContent = doc.data().name;
-    punteggio.textContent = doc.data().punteggio;
+  li.setAttribute('data-id', doc.id);
+  name.textContent = doc.data().name;
+  punteggio.textContent = doc.data().punteggio;
 
 
-    li.appendChild(name);
-    li.appendChild(punteggio);
-    pastalist.appendChild(li);
+  li.appendChild(name);
+  li.appendChild(punteggio);
+  pastalist.appendChild(li);
 }
 
-function draw(){
-background('yellow')
-textSize(50)
-text(timer, windowWidth/7*3,50)
-text( 'Penne: ' +  punteggiopenne, windowWidth/7*5,50)
-text('Spaghetti: ' + punteggiospaghetti, windowWidth/7*1,50)
+function draw() {
+  background('white')
+  textSize(50)
+  text(timer, windowWidth /2, 50)
+  text('Penne: ' + punteggiopenne, windowWidth / 7 * 5, 50)
+  text('Totale:' + scorepenne.punteggio, windowWidth / 7 * 5, 100)
+  text('Totale:' + scorespaghetti.punteggio, windowWidth / 7 * 2, 100)
+  text('Spaghetti: ' + punteggiospaghetti, windowWidth / 7 * 2, 50)
 
-if(punteggiopenne > punteggiospaghetti){
-  winner = 'Penne';
-}
-else{
-  winner = 'Spaghetti';
-}
+  text(winner + ' is winning', windowWidth /2, 175)
 
-if(timer <1){
-timer = 0;
-text(winner + ' wins',windowWidth/7*3,150)
-}
-}
+  db.collection("pasta").doc("penne")
+    .onSnapshot(function(doc) {
+      scorepenne = doc.data();
+    });
 
+  db.collection("pasta").doc("spaghetti")
+    .onSnapshot(function(doc) {
+      scorespaghetti = doc.data();
+    });
 
-function clickdx(){
-punteggiopenne+=1
-}
-
-function clicksx(){
-punteggiospaghetti+=1
-}
-
-function timerdown(){
-  if(timer <1){
-  timer = 0;
+  if (punteggiopenne > punteggiospaghetti) {
+    winner = 'Penne';
   }
-  else{
-  timer -=1}
+  else if(punteggiopenne == punteggiospaghetti){
+    winner = 'nobody'
+  }
+    else{
+    winner = 'Spaghetti';
+  }
+
+  if (timer < 1) {
+    timer = 0;
+  }
 }
 
 
-function mouseClicked(){
-  console.log(penne)
-if(mouseX > windowWidth/2){
-  socket.emit("mousedx");
-  punteggiopenne +=1
-  db.collection('pasta').doc('penne').update({
-  punteggio: increment
-})}
-if(mouseX < windowWidth/2){
-  socket.emit("mousesx");
-  punteggiospaghetti +=1
-  db.collection('pasta').doc('spaghetti').update({
-  punteggio: increment
-})}
+function clickdx() {
+  punteggiopenne += 1
+}
+
+function clicksx() {
+  punteggiospaghetti += 1
+}
+
+function timerdown() {
+  if (timer < 1) {
+    timer = 0;
+  } else {
+    timer -= 1
+  }
+}
+
+
+function mouseClicked() {
+  if(timer!=0){
+  if (mouseX > windowWidth / 2) {
+    socket.emit("mousedx");
+    punteggiopenne += 1
+    db.collection('pasta').doc('penne').update({
+      punteggio: increment
+    })
+  }
+  if (mouseX < windowWidth / 2) {
+    socket.emit("mousesx");
+    punteggiospaghetti += 1
+    db.collection('pasta').doc('spaghetti').update({
+      punteggio: increment
+    })
+  }}
 
 }
